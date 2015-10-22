7b4f218028329cbfb2991b8595d16b64bdbdf594
Luke Wendling <luke@lukewendling.com>
2015-10-22 11:34:41 -0500

    use LiveSet to wrap the streaming changes

--- client/js/controllers/todo.js
+++ client/js/controllers/todo.js
@@ -1,53 +1,43 @@
 angular
   .module('app')
-  .controller('TodoController', ['$scope', '$state', 'Todo','createChangeStream', function($scope,
-      $state, Todo,createChangeStream) {
-    $scope.todos = [];
+  .controller('TodoController', ['$scope', 'Todo', 'createChangeStream', 'LiveSet',
+    function($scope, Todo, createChangeStream, LiveSet) {
+      $scope.todos = [];
 
-    var src = new EventSource('/api/Todos/change-stream?_format=event-source');
-    var changes = createChangeStream(src);
+      var src = new EventSource('/api/Todos/change-stream?_format=event-source');
+      var changes = createChangeStream(src);
+      var todoSet;
 
-    changes.on('data', function(update) {
-      console.log(update);
-    });
-
-    /*var urlToChangeStream = '/api/Todos/change-stream?_format=event-source';
-    var src = new EventSource(urlToChangeStream);
-    src.addEventListener('data', function(msg) {
-      var data = JSON.parse(msg.data);
-      console.log(data); // the change object
-    });*/
-
-    function getTodos() {
-      Todo
-        .find()
-        .$promise
-        .then(function(results) {
-          $scope.todos = results;
-        });
-    }
-    getTodos();
+      function getTodos() {
+        Todo
+          .find()
+          .$promise
+          .then(function(todos) {
+            todoSet = new LiveSet(todos, changes);
+            $scope.todos = todoSet.toLiveArray();
+          });
+      }
+      getTodos();
 
 
+      $scope.addTodo = function() {
+        Todo
+          .create($scope.newTodo)
+          .$promise
+          .then(function(todo) {
+            $scope.newTodo = '';
+            $scope.todoForm.content.$setPristine();
+            $('.focus').focus();
+            getTodos();
+          });
+      };
 
-    $scope.addTodo = function() {
-      Todo
-        .create($scope.newTodo)
-        .$promise
-        .then(function(todo) {
-          $scope.newTodo = '';
-          $scope.todoForm.content.$setPristine();
-          $('.focus').focus();
-          getTodos();
-        });
-    };
-
-    $scope.removeTodo = function(item) {
-      Todo
-        .deleteById(item)
-        .$promise
-        .then(function() {
-          getTodos();
-        });
-    };
+      $scope.removeTodo = function(item) {
+        Todo
+          .deleteById(item)
+          .$promise
+          .then(function() {
+            getTodos();
+          });
+      };
   }]);
